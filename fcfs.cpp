2654915c0919de3ef96ea70f76070b5f62a71662
R K S <ramakantasahoo1202@gmail.com>
2023-11-03 19:42:15 +0530

    03-11-2023

--- fcfs.cpp
+++ fcfs.cpp
@@ -1,50 +1,72 @@
 // Write a program to implement first come first serve cpu scheduling algorithm (FCFS) with arrival time.
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    int n, bt[20], at[20], wt[20], tat[20], temp[20];
-    float awt = 0;
-    float atat = 0;
+    int n;
     cout << "Enter the number of processes: ";
     cin >> n;
-    cout << "Enter the burst time of the processes: ";
-    for (int i = 0; i < n; i++)
-        cin >> bt[i];
-
-    cout << "Enter the arrival time of the processes: ";
-    for (int i = 0; i < n; i++)
-        cin >> at[i];
-
-    temp[0] = 0;
-    cout << "Process\t Burst time\t Arrival time\t Waiting time\t Turn around time" << endl;
+    int pid[n], at[n], bt[n], tat[n], wt[n], ct[n];
+    cout << "Enter processId, Arrival time and Burst time of the program: " << endl;
     for (int i = 0; i < n; i++)
     {
-        wt[i] = 0;
-        tat[i] = 0;
-        temp[i + 1] = temp[i] + bt[i];
-        wt[i] = temp[i] - at[i];
-        tat[i] = wt[i] + bt[i];
-        awt = awt + wt[i];
-        atat = atat + tat[i];
-        cout << i + 1 << "\t" << bt[i] << "\t\t" << at[i] << "\t\t" << wt[i] << "\t\t" << tat[i] << endl;
+        cin >> pid[i] >> at[i] >> bt[i];
     }
-    awt = awt / n;
-    atat = atat / n;
-    cout << "Average waiting time and avegate turn around time is: " << awt << " and " << atat << " respectively" << endl;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (at[j] > at[j + 1])
+            {
+                swap(pid[j], pid[j + 1]);
+                swap(at[j], at[j + 1]);
+                swap(bt[j], bt[j + 1]);
+            }
+        }
+    }
+
+    int temp = at[0];
+    for (int i = 0; i < n; i++)
+    {
+        temp += bt[i];
+        ct[i] = temp;
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+    }
+    float avg_tat = 0, avg_wt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        avg_tat += tat[i];
+        avg_wt += wt[i];
+    }
+    avg_tat = avg_tat / (float)n;
+    avg_wt = avg_wt / (float)n;
+
+    cout << "\t PId \t AT \t BT \t CT \t TAT \t WT" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\t" << pid[i] << "\t" << at[i] << "\t" << bt[i] << "\t" << ct[i] << "\t" << tat[i] << "\t" << wt[i] << endl;
+    }
+    cout << endl;
+    cout << "Average turn around time and average waiting time are respectively: " << avg_tat << " and " << avg_wt << endl;
     return 0;
 }
 
 // Terminal
 
 // Enter the number of processes: 4
-// Enter the burst time of the processes: 5 4 1 3
-// Enter the arrival time of the processes: 0 1 2 3
-// Process  Burst time      Arrival time    Waiting time    Turn around time
-// 1       5               0               0               5
-// 2       4               1               4               8
-// 3       1               2               7               8
-// 4       3               3               7               10
-// Average waiting time and avegate turn around time is: 4.5 and 7.75 respectively
+// Enter processId, Arrival time and Burst time of the program:
+// 1 0 10
+// 2 1 6
+// 3 3 2
+// 4 5 4
+//          PId     AT      BT      CT      TAT     WT
+//         1       0       10      10      10      0
+//         2       1       6       16      15      9
+//         3       3       2       18      15      13
+//         4       5       4       22      17      13
+
+// Average turn around time and average waiting time are respectively: 14.25 and 8.75
